e7363beabda10f8b1fdc8c1f9d46916c7e1c79ca
nagi <natsag2000@gmail.com>
2010-09-03 12:19:53 +0200

    added convert script for all dot files

--- fexporter/src/main.erl
+++ fexporter/src/main.erl
@@ -1,5 +1,13 @@
+%%  __  __  ___  _   _  ____  ___  _     ____ ___ ____ _   _ ___ ____
+%% |  \/  |/ _ \| \ | |/ ___|/ _ \| |   | __ )_ _/ ___| | | |_ _/ ___|
+%% | |\/| | | | |  \| | |  _| | | | |   |  _ \| | |   | |_| || | |  _
+%% | |  | | |_| | |\  | |_| | |_| | |___| |_) | | |___|  _  || | |_| |
+%% |_|  |_|\___/|_| \_|\____|\___/|_____|____/___\____|_| |_|___\____|
+%%
+
 -module(main).
--compile([export_all]).
+-export([start/0]).
+
 -import(config, [read/0, get/2]).
 -import(fextractor, [extract_feature/1]).
 -import(fea2dot, [export_dot/3]).
@@ -12,12 +20,18 @@ start() ->
     {ok, FFolder} = get(feature_folder, Terms),
     {ok, CFolder} = get(class_folder, Terms),
     {ok, EFolder} =get(export_folder, Terms),
-    Filename = filename:join([ Root, FFolder, ?testfile]),
-    io:format("FFolder: ~p~nEFolder: ~p~nFilename: ~p~n",
-              [FFolder, EFolder, Filename]),
-    {ok, {feature, Name, Features}} = extract_feature(Filename),
+    {ok, FList} = file:list_dir(filename:join(Root, FFolder)),
+    doit(FList, Root, FFolder, CFolder, EFolder).
+
+doit([], _, _, _, _) ->
+    done;
+doit(["aalt"|Rest], R, FF, CF, EF) ->
+    doit(Rest, R, FF, CF, EF);
+doit([Filename|Rest], R, FF, CF, EF) ->
+    FFile = filename:join([R,FF,Filename]),
+    {ok, {feature, Name, Features}} = extract_feature(FFile),
     io:format("Feature: ~p, feature count:~p~n", [Name, length(Features)]),
     export_dot({feature, Name, Features},
-               filename:join([Root, CFolder]),
-               filename:join([Root, EFolder])).
-
+               filename:join([R, CF]),
+               filename:join([R, EF])),
+    doit(Rest, R, FF, CF, EF).
