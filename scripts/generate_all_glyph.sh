d46c04c3040b6579d9f03d589d360a616b5aee27
nagi <nagi@Nagis-Mac-2.box>
2010-09-04 01:48:51 +0200

    usable version

--- scripts/generate_all_glyph.sh
+++ scripts/generate_all_glyph.sh
@@ -23,12 +23,21 @@ set glyphfolder=$3
 
 set classfolder=$fontfolder/classes
 set featurefolder=$fontfolder/features
+set miscfolder=../misc
 
-set classglyphs=../test/classglyphs
-set featureglyphs=../test/featureglyphs
-set allglyphs=../test/allglyphs
+set classglyphs=$miscfolder/classglyphs
+set featureglyphs=$miscfolder/featureglyphs
+set replaceglyphs=$miscfolder/replaceglyphs
+set allglyphs=$miscfolder/allglyphs
 
 set tmpfile=/tmp/test
+
+# first clean!
+[ -f $classglyphs ] && rm $classglyphs
+[ -f $featureglyphs ] && rm $featureglyphs
+[ -f $replaceglyphs ] && rm $replaceglyphs
+[ -f $allglyphs ] && rm $allglyphs
+[ ! -d $miscfolder ] && mkdir $miscfolder
 
 # create glyphs list from classfolder
 # ===================================
@@ -37,13 +46,14 @@ foreach F (`find $classfolder -type f`)
 end
 cat $tmpfile | sort -u > $classglyphs
 
-# create glyphs list from featurefolder
-# =====================================
+# create featureglyphs and replaceglyphs list from featurefolder
+# ==============================================================
 rm $tmpfile
 foreach F (`find $featurefolder -type f`)
     cat $F >> $tmpfile
 end
 cat $tmpfile | grep uni | tr ' ' '\n' | grep uni | tr ';' ' ' | tr "'" ' ' | tr ']' ' ' | tr '[' ' ' | sed 's/ //g' | sort -u > $featureglyphs
+cat $tmpfile | grep uni | grep by | sed 's/by/:/' | cut -d":" -f2 | tr ' ' '\n' | grep uni | tr ';' ' ' | tr "'" ' ' | tr ']' ' ' | tr '[' ' ' | sed 's/ //g' | sort -u > $replaceglyphs
 
 # create all glyphs
 # ==================
@@ -63,7 +73,7 @@ foreach G (`cat $allglyphs`)
 end
 
 foreach File (*.bmp)
-  set newname=`basename $File | sed 's/\$//' | sed 's/.bmp/.png/'`
+  set newname=`basename $File | sed 's/\$//g' | sed 's/.bmp/.png/'`
   convert $File $glyphfolder/$newname
   rm $File
 end
