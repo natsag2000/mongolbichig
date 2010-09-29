a9514d5192274f53edd808eeab081d247c1f8a01
nagi <natsag2000@gmail.com>
2010-09-29 09:35:36 +0200

    small struktur changes on crazyfonter.py

--- fontcreator/crazyfonter.py
+++ fontcreator/crazyfonter.py
@@ -2,71 +2,141 @@
 #
 # crazy font creator
 #
-import fontforge 
+import fontforge
 import ConfigParser
+import os
 
-# read config files
-config = ConfigParser.RawConfigParser()
-config.read("config.cfg")
-
-# TODO: check if they exist!
-MONGOL_USEG     = config.get('Font data', 'MONGOL_USEG')
-MONGOL_USEG_EXT = config.get('Font data', 'MONGOL_USEG_EXT')
-GLYPHS_DIR      = config.get('Font data', 'GLYPHS_DIR')
-BASE_FONT       = config.get('Font data', 'BASE_FONT')
-FEATURE_FILE    = config.get('Font data', 'FEATURE_FILE')
-
+# GLOBALS
+MONGOL_USEG      = ''
+MONGOL_USEG_EXT  = ''
+GLYPHS_DIR       = ''
+BASE_FONT        = ''
+FEATURE_FILE     = ''
+FONTNAME         = ''
+FAMILYNAME       = ''
+FULLNAME         = ''
+VERSION          = ''
+FONT             = ''
 # internal use
 TMP_FONT = '/tmp/temp.sfd'
-
 #BLANK_FONT = '../resources/blank.sfd'
-
 # private usage zone
 PRIVATE_ZONE =hex(0xf300)
 
-font1 = fontforge.open(BASE_FONT)
-# Delta char has conflict by otf generation, so delete it
-font1["Delta"].clear()
-font1.generate(TMP_FONT)
-font = fontforge.open(TMP_FONT)
+def main():
+    global MONGOL_USEG
+    global MONGOL_USEG_EXT
+    read_config()
+    mongoluseg = read_lines(MONGOL_USEG)
+    mongoluseg_ext = read_lines(MONGOL_USEG_EXT)
+    prepare_font()
+    create_standart_zone(mongoluseg)
+    create_private_zone(mongoluseg_ext)
+    replace_feature()
+    generate_font("crazyfont.ttf")
 
-f = file(MONGOL_USEG, 'r')
-mongoluseg = f.readlines()
-f.close()
-f = file(MONGOL_USEG_EXT, 'r')
-mongoluseg_ext = f.readlines()
-f.close()
+def prepare_font():
+    global BASE_FONT
+    global TMP_FONT
+    global FONT
+    fontbase = fontforge.open(BASE_FONT)
+    # Delta char has conflict by otf generation, so delete it
+    fontbase["Delta"].clear()
+    fontbase.generate(TMP_FONT)
+    FONT = fontforge.open(TMP_FONT)
+
+def read_config():
+    global MONGOL_USEG
+    global MONGOL_USEG_EXT
+    global GLYPHS_DIR
+    global BASE_FONT
+    global FEATURE_FILE
+    global FONTNAME
+    global FAMILYNAME
+    global FULLNAME
+    global VERSION
+    # read config files
+    config = ConfigParser.RawConfigParser()
+    config.read("config.cfg")
+    # TODO: check if they exist!
+    MONGOL_USEG      = config.get('Font creator', 'MONGOL_USEG')
+    MONGOL_USEG_EXT  = config.get('Font creator', 'MONGOL_USEG_EXT')
+    GLYPHS_DIR       = config.get('Font creator', 'GLYPHS_DIR')
+    BASE_FONT        = config.get('Font creator', 'BASE_FONT')
+    FEATURE_FILE     = config.get('Font creator', 'FEATURE_FILE')
+    FONTNAME         = config.get('General', 'FONTNAME')
+    FAMILYNAME       = config.get('General', 'FAMILYNAME')
+    FULLNAME         = config.get('General', 'FULLNAME')
+    VERSION          = config.get('General', 'VERSION')
+
+def read_lines(FileName):
+    f = file(FileName, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
 
 # first unicode standart zone
 # TODO check special cases: maaglha, dottedcircle etc!!
-for letter in mongoluseg:
-    letter = letter.strip()
-    unizone=letter.split("uni")[1]
-    ucode=int(unizone,16)
-    # make new glyph
-    #font.createMappedChar(letter) ##not works!
-    font.createChar(ucode,letter)
-    # import outline file
-    # notice that font[glyphname] returns the appropriate glyph
-    fontpath = GLYPHS_DIR + '/' +letter+".svg"
-    font[letter].importOutlines(fontpath)
-    # TODO same spacing for each letter, this is a hack after all TODO!!
-    font[letter].left_side_bearing = 15
-    font[letter].right_side_bearing = 15
+def create_standart_zone(Usegs):
+    global GLYPHS_DIR
+    global FONT
+    for letter in Usegs:
+        letter = letter.strip()
+        unizone=letter.split("uni")[1]
+        ucode=int(unizone,16)
+        FONT.createChar(ucode,letter)
+        # import outline file
+        # notice that font[glyphname] returns the appropriate glyph
+        fontpath = GLYPHS_DIR + '/' +letter+".svg"
+        FONT[letter].importOutlines(fontpath)
+        # TODO same spacing for each letter, this is a hack after all TODO!!
+        FONT[letter].left_side_bearing = 15
+        FONT[letter].right_side_bearing = 15
 
-# private zone
-deccounter=int(PRIVATE_ZONE, 16)
-for letter in mongoluseg_ext:
-    letter = letter.strip()
-    font.createChar(deccounter, letter)
-    fontpath = GLYPHS_DIR + '/' +letter+".svg"
-    print(fontpath)
-    font[letter].importOutlines(fontpath)
-    font[letter].left_side_bearing = -15
-    font[letter].right_side_bearing = -15
-    deccounter = deccounter + 1
+def create_private_zone(Usegs):
+    global PRIVATE_ZONE
+    global GLYPHS_DIR
+    global FONT
+    deccounter=int(PRIVATE_ZONE, 16)
+    for letter in Usegs:
+        letter = letter.strip()
+        FONT.createChar(deccounter, letter)
+        fontpath = GLYPHS_DIR + '/' +letter+".svg"
+        if not os.path.exists(fontpath):
+            print "Glyph image file not found: ", fontpath
+            continue
+        FONT[letter].importOutlines(fontpath)
+        FONT[letter].left_side_bearing = -15
+        FONT[letter].right_side_bearing = -15
+        deccounter = deccounter + 1
 
-# merge feature
-font.mergeFeature(FEATURE_FILE)
-# generate font
-font.generate('crazy.ttf')
+# replace feature
+def replace_feature(Fea = None):
+    global FONT
+    global FEATURE_FILE
+    Feature = None
+    if Fea is None:
+        Feature = FEATURE_FILE
+    else:
+        Feature = Fea
+    for F in FONT.gsub_lookups:
+        FONT.removeLookup(F)
+    if not os.path.exists(Feature):
+        print "Feature file is not found: ", Feature
+    else:
+        FONT.mergeFeature(Feature)
+
+def generate_font(FontFileName):
+    global FONTNAME
+    global FAMILYNAME
+    global FULLNAME
+    global VERSION
+    global FONT
+    FONT.fullname = FULLNAME
+    FONT.familyname = FAMILYNAME
+    FONT.version = VERSION
+    FONT.fontname = FONTNAME
+    FONT.generate(FontFileName)
+
+if __name__ == '__main__':
+    main()
